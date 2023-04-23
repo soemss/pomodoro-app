38452042784b732572a34e07db9ecf6b6cd6f025
Soems <email>
2023-04-23 01:51:44 -0700

    ujh

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Timer from './comp/timer/timer';
 import { StyledEngineProvider } from '@mui/material';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { useEffect } from 'react';
+import Spotify from 'react-spotify-embed';
 
 function App() {
   const handle = useFullScreenHandle();
@@ -22,6 +23,7 @@ function App() {
         <FullScreen handle={handle}>
           <Timer/>
           {/* <Settings/> */}
+          <Spotify className='spotify' wide link='https://open.spotify.com/playlist/0vvXsWCC9xrXsKd4FyS8kM?si=a2d073683b1f49ad'/>
         </FullScreen>
       </StyledEngineProvider>
     </div>
